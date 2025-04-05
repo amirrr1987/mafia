04befbcdcc61c6d6d58c34958168d76ea8aa68d7
Amir Maghami <Maghami.A1987@gmail.com>
2025-04-05 11:16:03 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <nav class="bg-red-500">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+  <nav class="py-6 bg-red-500">
+    <div class="container mx-auto px-4">
+      <ul class="flex gap-4">
+        <RouterLink :to="{ name: 'PlayerView' }" class="text-white" active-class="text-yellow-500">
+          PlayerView
+        </RouterLink>
+        <RouterLink :to="{ name: 'RoleView' }" class="text-white" active-class="text-yellow-500">
+          RoleView
+        </RouterLink>
+        <RouterLink :to="{ name: 'GameView' }" class="text-white" active-class="text-yellow-500">
+          GameView
+        </RouterLink>
+      </ul>
+    </div>
   </nav>
   <RouterView />
 </template>

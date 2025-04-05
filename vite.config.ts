01a8f1d74f2c0b5effacb074e890cf4806d9e03c
Amir Maghami <Maghami.A1987@gmail.com>
2025-04-05 15:11:27 +0330

    feat: icons for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })

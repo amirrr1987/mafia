f02b789bead18abf4bd5aa2621d103ae8c8bf9db
Amir Maghami <Maghami.A1987@gmail.com>
2025-05-04 18:57:22 +0330

    Update RolesView.vue

--- src/views/RolesView.vue
+++ src/views/RolesView.vue
@@ -3,21 +3,19 @@
     <div class="grid grid-cols-2 gap-4">
       <CheckboxGroup v-for="role in mafiaStore.roleList" :key="role.role">
         <Checkbox class="">
-          <Card class="text-wrap" :title="role.label">
-            <template #extra>
-              <Avatar :src="role.avatar" />
-            </template>
-            {{ role.description }}
-          </Card>
+          <RoleCard class="text-wrap" :title="role.label">
+            {{ truncate(role.description, { length: 100 }) }}
+          </RoleCard>
         </Checkbox>
       </CheckboxGroup>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import RoleCard from '@/components/RoleCard.vue'
 import { useMafiaStore } from '@/stores/mafia.store'
-import { Checkbox, CheckboxGroup, Avatar } from 'ant-design-vue/es'
-import { Card } from 'ant-design-vue/es'
+import { Checkbox, CheckboxGroup } from 'ant-design-vue/es'
+import { truncate } from 'lodash'
 const mafiaStore = useMafiaStore()
 </script>
 <style lang="less">
@@ -35,8 +33,9 @@ const mafiaStore = useMafiaStore()
     &.ant-checkbox-wrapper-checked {
       span {
         &:nth-child(2) {
-          .ant-card-body {
-            @apply rounded bg-primary;
+          .role-card {
+            @apply bg-primary;
+            background-color: red;
           }
         }
       }

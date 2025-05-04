0a236abe07a396903bcc6ac176afb01adc785479
Amir Maghami <Maghami.A1987@gmail.com>
2025-05-04 19:15:26 +0330

    Update RoleCard.vue

--- src/components/RoleCard.vue
+++ src/components/RoleCard.vue
@@ -5,7 +5,10 @@
     <template #cover>
       <img
         alt="example"
-        src="https://images-cdn.fantasyflightgames.com/filer_public/24/7d/247d13ce-5542-43b7-82fa-0e8e317c4028/godfathercard.png"
+        :src="
+          props.avatar ??
+          'https://images-cdn.fantasyflightgames.com/filer_public/24/7d/247d13ce-5542-43b7-82fa-0e8e317c4028/godfathercard.png'
+        "
       />
     </template>
     <template #default>
@@ -19,12 +22,7 @@ import { ref } from 'vue'
 const roleCardRef = ref<HTMLElement | null>(null)
 interface Props {
   title?: string
-  hasFullScreen?: boolean
-  isLoading?: boolean
-  icon?: {
-    name: string
-    class?: string
-  }
+  avatar?: string
 }
 const props = withDefaults(defineProps<Props>(), {})
 </script>
